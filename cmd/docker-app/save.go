b8c5d451a8b6033efb8cc84e6ac7854b1b815a53
Antony MECHIN <antony.mechin@docker.com>
2018-06-07 15:20:05 +0200

    cmd.save: Add missing whitespace.
    Signed-off-by: Antony MECHIN <antony.mechin@docker.com>

--- cmd/docker-app/save.go
+++ cmd/docker-app/save.go
@@ -17,7 +17,7 @@ func saveCmd() *cobra.Command {
 	var opts saveOptions
 	cmd := &cobra.Command{
 		Use:   "save [<app-name>]",
-		Short: "Save the application as an image to the docker daemon(in preparation for push).",
+		Short: "Save the application as an image to the docker daemon (in preparation for push).",
 		Args:  cli.RequiresMaxArgs(1),
 		RunE: func(cmd *cobra.Command, args []string) error {
 			imageName, err := packager.Save(firstOrEmpty(args), opts.namespace, opts.tag)
